6469ab9b52ae35d03b31661ae381fd499ce55246
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2025-09-01 14:23:26 +0700

    Anam

--- Assets/Scripts/Anam's Script/Menu's Script/ExitButton.cs
+++ Assets/Scripts/Anam's Script/Menu's Script/ExitButton.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class ExitButton : MonoBehaviour
 {
-    // Fungsi publik tanpa parameter, Unity bisa panggil dari Button OnClick
+   
     public void ExitGame()
     {
         Debug.Log("[ExitButton] Quit game called.");
 
 #if UNITY_EDITOR
-        // Kalau lagi di Editor → stop Play mode
+       
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-        // Kalau di build (exe/apk) → keluar aplikasi
+        
         Application.Quit();
 #endif
     }
