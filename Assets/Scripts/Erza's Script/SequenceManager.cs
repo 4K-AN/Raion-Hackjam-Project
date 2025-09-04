a4ffcf335a4e308dd735ec58b04cb1505684d643
Akhmad Syafiul Anam <akhmadsyafiul01@gmail.com>
2025-09-05 00:18:21 +0700

    hmm

--- Assets/Scripts/Erza's Script/SequenceManager.cs
+++ Assets/Scripts/Erza's Script/SequenceManager.cs
@@ -3,30 +3,43 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 using UnityEngine.UI;
+using System.Linq; // Diperlukan untuk OrderBy
+
+// "Wadah" untuk pasangan Key dan Sprite. Letakkan di luar kelas utama.
+[System.Serializable]
+public class KeySpritePair
+{
+    public Key key;
+    public Sprite sprite;
+}
 
 public class SequenceManager : MonoBehaviour
 {
-    [Header("Sequence Settings")]
+    [Header("Pengaturan Urutan")]
     [SerializeField] private int sequenceLength = 5;
     [SerializeField] private float inputTimeLimit = 10f; 
     
-    [Header("UI Elements")]
-    public TMP_Text p1SequenceList;
-    public TMP_Text p2SequenceList;
-    public TMP_Text p1Progress;
-    public TMP_Text p2Progress;
-    public Transform p1SequenceContainer;
-    public Transform p2SequenceContainer;
-    public GameObject keyIconPrefab;
-    public List<KeySpritePair> keySpritePairs;
+    [Header("Elemen UI")]
+    public Transform p1SequenceContainer; // Parent untuk ikon-ikon P1
+    public Transform p2SequenceContainer; // Parent untuk ikon-ikon P2
+    public GameObject keyIconPrefab;      // Prefab untuk ikon tombol
+    
+    [Header("Pengaturan Tombol & Sprite")]
+    // Ini adalah dua daftar yang seharusnya muncul di Inspector
+    public List<KeySpritePair> player1KeyMap;
+    public List<KeySpritePair> player2KeyMap;
 
-    private Dictionary<Key, Sprite> keySpriteMap;
-
-    [Header("Visual Feedback")]
+    [Header("Umpan Balik Visual")]
     public Color correctColor = Color.green;
     public Color incorrectColor = Color.red;
     public Color pendingColor = Color.white;
     
+    // Event untuk GameManager
+    public System.Action<int> OnPlayerWinsClash;
+    public System.Action<int> OnPlayerFailsSequence;
+
+    // Variabel Internal
+    private Dictionary<Key, Sprite> keySpriteMap;
     private List<Key> p1Sequence;
     private List<Key> p2Sequence;
     private List<Image> p1Icons;
@@ -37,239 +50,123 @@ public class SequenceManager : MonoBehaviour
     private float p2Timer;
     private bool p1Active;
     private bool p2Active;
-    
-    
-    private Key[] p1Keys = { Key.Q, Key.W, Key.E, Key.A, Key.S, Key.D, Key.Z, Key.X, Key.C };
-    private Key[] p2Keys = { Key.I, Key.O, Key.P, Key.J, Key.K, Key.L, Key.N, Key.M, Key.Comma };
-    
-   
-    public System.Action<int> OnPlayerWinsClash;
-    public System.Action<int> OnPlayerFailsSequence;
 
     private void Awake()
     {
         keySpriteMap = new Dictionary<Key, Sprite>();
-        foreach (var pair in keySpritePairs)
+        foreach (var pair in player1KeyMap)
+        {
+            if (!keySpriteMap.ContainsKey(pair.key))
+                keySpriteMap.Add(pair.key, pair.sprite);
+        }
+        foreach (var pair in player2KeyMap)
         {
             if (!keySpriteMap.ContainsKey(pair.key))
                 keySpriteMap.Add(pair.key, pair.sprite);
         }
     }
+    
     private void Update()
     {
-        
-        if (p1Active)
-        {
-            p1Timer -= Time.deltaTime;
-            if (p1Timer <= 0f)
-            {
-                Debug.Log("Player 1 ran out of time!");
-                OnPlayerFailsSequence?.Invoke(1);
-                p1Active = false;
-            }
-        }
-        
-        if (p2Active)
-        {
-            p2Timer -= Time.deltaTime;
-            if (p2Timer <= 0f)
-            {
-                Debug.Log("Player 2 ran out of time!");
-                OnPlayerFailsSequence?.Invoke(2);
-                p2Active = false;
-            }
-        }
+        if (p1Active) { p1Timer -= Time.deltaTime; if (p1Timer <= 0f) { FailSequence(1); } }
+        if (p2Active) { p2Timer -= Time.deltaTime; if (p2Timer <= 0f) { FailSequence(2); } }
     }
 
     public void StartClashP1()
     {
-        p1Sequence = GenerateRandomSequence(p1Keys, sequenceLength);
+        p1Sequence = GenerateRandomSequence(player1KeyMap, sequenceLength);
         p1Index = 0;
         p1Timer = inputTimeLimit;
         p1Active = true;
-
-        DisplaySequence(p1Sequence, p1SequenceContainer, ref p1Icons);
-        UpdateSequenceDisplay(1);
-        Debug.Log("Player 1 sequence: " + string.Join(", ", p1Sequence));
+        DisplaySequenceIcons(p1Sequence, p1SequenceContainer, ref p1Icons);
     }
 
     public void StartClashP2()
     {
-        p2Sequence = GenerateRandomSequence(p2Keys, sequenceLength);
+        p2Sequence = GenerateRandomSequence(player2KeyMap, sequenceLength);
         p2Index = 0;
         p2Timer = inputTimeLimit;
         p2Active = true;
-
-        DisplaySequence(p2Sequence, p2SequenceContainer, ref p2Icons);
-        UpdateSequenceDisplay(2);
-        Debug.Log("Player 2 sequence: " + string.Join(", ", p2Sequence));
+        DisplaySequenceIcons(p2Sequence, p2SequenceContainer, ref p2Icons);
     }
 
     public bool ProcessInput(int playerID, Key key)
     {
         if (playerID == 1 && p1Active)
         {
-            return ProcessPlayerInput(ref p1Index, p1Sequence, p1Icons, key, 1);
+            if (key == p1Sequence[p1Index])
+            {
+                p1Icons[p1Index].color = correctColor;
+                p1Index++;
+                if (p1Index >= sequenceLength) WinClash(1);
+            }
+            else
+            {
+                p1Icons[p1Index].color = incorrectColor;
+                FailSequence(1);
+            }
+            return true;
         }
         else if (playerID == 2 && p2Active)
         {
-            return ProcessPlayerInput(ref p2Index, p2Sequence, p2Icons, key, 2);
+            if (key == p2Sequence[p2Index])
+            {
+                p2Icons[p2Index].color = correctColor;
+                p2Index++;
+                if (p2Index >= sequenceLength) WinClash(2);
+            }
+            else
+            {
+                p2Icons[p2Index].color = incorrectColor;
+                FailSequence(2);
+            }
+            return true;
         }
-        
         return false;
     }
 
-    private bool ProcessPlayerInput(ref int index, List<Key> sequence, List<Image> icons, Key inputKey, int playerID)
+    private void WinClash(int playerID)
     {
-        if (index >= sequence.Count) return false;
-
-        if (index == 0 && inputKey != sequence[0])
-        {
-        Debug.Log($"Player {playerID} pressed {inputKey}, waiting for {sequence[0]} to start sequence...");
-        return false;
-        }
-
-        if (inputKey == sequence[index])
-        {
-            Debug.Log($"Player {playerID} correct input: {inputKey} ({index}/{sequence.Count})");
-            
-            UpdateProgressDisplay(playerID, index, sequence.Count);
-
-            icons[index].color = correctColor;
-            index++;
-            if (index >= sequence.Count)
-            {
-                Debug.Log($"Player {playerID} menang clash!");
-                if (playerID == 1) p1Active = false;
-                else p2Active = false;
-                return true;
-            }
-
-            if (index >= sequence.Count)
-            {
-                Debug.Log($"Player {playerID} wins the clash!");
-                if (playerID == 1) p1Active = false;
-                else p2Active = false;
-                
-                OnPlayerWinsClash?.Invoke(playerID);
-                return true;
-            }
-        }
-        else
-        {
-            Debug.Log($"Player {playerID} incorrect input: {inputKey} (expected: {sequence[index]})");
-            icons[index].color = incorrectColor;
-        }
-        
-        return false;
+        p1Active = false;
+        p2Active = false;
+        OnPlayerWinsClash?.Invoke(playerID);
     }
 
-    private void DisplaySequence(List<Key> sequence, Transform container, ref List<Image> iconList)
+    private void FailSequence(int playerID)
     {
-        foreach (Transform child in container)
-            Destroy(child.gameObject);
+        p1Active = false;
+        p2Active = false;
+        OnPlayerFailsSequence?.Invoke(playerID);
+    }
 
-        iconList = new List<Image>();
+    private List<Key> GenerateRandomSequence(List<KeySpritePair> keyMap, int length)
+    {
+        var keyPool = keyMap.Select(pair => pair.key).ToList();
+        return keyPool.OrderBy(x => System.Guid.NewGuid()).Take(length).ToList();
+    }
+    
+    private void DisplaySequenceIcons(List<Key> sequence, Transform container, ref List<Image> iconList)
+    {
+        foreach (Transform child in container) Destroy(child.gameObject);
+        if(iconList != null) iconList.Clear(); else iconList = new List<Image>();s
 
         foreach (Key k in sequence)
         {
             GameObject iconGO = Instantiate(keyIconPrefab, container);
             Image img = iconGO.GetComponent<Image>();
-
             if (keySpriteMap.TryGetValue(k, out Sprite sp))
-            {
-                img.sprite = sp;
-                img.color = pendingColor;
-                img.enabled = true;
-            }
-            else
-            {
-                img.enabled = false;
-            }
-
-            img.color = pendingColor; 
+{
+    img.sprite = sp; // <-- TAMBAHKAN TITIK KOMA DI SINI
+    img.color = pendingColor;
+}
             iconList.Add(img);
-        }
-    }
-    private List<Key> GenerateRandomSequence(Key[] keyPool, int length)
-    {
-        List<Key> sequence = new List<Key>();
-        for (int i = 0; i < length; i++)
-        {
-            int randomIndex = Random.Range(0, keyPool.Length);
-            sequence.Add(keyPool[randomIndex]);
-        }
-        return sequence;
-    }
-    
-    private void UpdateSequenceDisplay(int playerID)
-    {
-        if (playerID == 1 && p1SequenceList != null)
-        {
-            p1SequenceList.text = string.Join(" ", p1Sequence);
-        }
-        else if (playerID == 2 && p2SequenceList != null)
-        {
-            p2SequenceList.text = string.Join(" ", p2Sequence);
-        }
-    }
-    
-    private void UpdateProgressDisplay(int playerID, int currentIndex, int totalLength)
-    {
-        string progressText = $"{currentIndex}/{totalLength}";
-        
-        if (playerID == 1 && p1Progress != null)
-        {
-            p1Progress.text = progressText;
-        }
-        else if (playerID == 2 && p2Progress != null)
-        {
-            p2Progress.text = progressText;
         }
     }
 
     public void ClearSequences()
     {
         p1Active = p2Active = false;
-        p1Timer = p2Timer = 0f;
-        
-        if (p1SequenceList != null) p1SequenceList.text = "";
-        if (p2SequenceList != null) p2SequenceList.text = "";
-        if (p1Progress != null) p1Progress.text = "";
-        if (p2Progress != null) p2Progress.text = "";
-
-        if (p1SequenceContainer != null)
-        {
-            foreach (Transform child in p1SequenceContainer)
-            {
-                Destroy(child.gameObject);
-            }
-        }
-
-        if (p2SequenceContainer != null)
-        {
-            foreach (Transform child in p2SequenceContainer)
-            {
-                Destroy(child.gameObject);
-            }
-        }
+        if (p1SequenceContainer != null) foreach (Transform child in p1SequenceContainer) Destroy(child.gameObject);
+        if (p2SequenceContainer != null) foreach (Transform child in p2SequenceContainer) Destroy(child.gameObject);
     }
-    
-   
-    public bool IsPlayerActive(int playerID)
-    {
-        return playerID == 1 ? p1Active : p2Active;
-    }
-    
-    public float GetPlayerTimeRemaining(int playerID)
-    {
-        return playerID == 1 ? p1Timer : p2Timer;
-    }
-}
-
-[System.Serializable]
-public class KeySpritePair
-{
-    public Key key;
-    public Sprite sprite;
 }
